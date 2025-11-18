6067562eacb5056e62d63f791d83c8aff9a98aee
Klopov Vladimir <faliingsky@mail.ru>
2025-11-18 17:54:25 +0400

    create users pagination

--- src/modules/user-accounts/api/user.controllers.ts
+++ src/modules/user-accounts/api/user.controllers.ts
@@ -1,23 +1,36 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Query } from '@nestjs/common';
 import { UsersInputDto } from './input-dto/users.input-dto';
 import { UserService } from '../application/user.service';
+import { UserQueryRepository } from '../infrastructure/user.query-repository';
+import { UserViewDto } from './view-dto/user-view-dto';
+import { PaginatedViewDto } from '../../../core/dto/base.paginated.view-dto';
+import { GetUsersQueryParams } from './view-dto/get-users-query-params';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly userQueryRepository: UserQueryRepository,
+  ) {}
 
   @Get()
-  async getUsers() {
-    return await this.userService.findAllUsers();
+  async getAllUsers(
+    @Query() query: GetUsersQueryParams,
+  ): Promise<PaginatedViewDto<UserViewDto[]>> {
+    return this.userQueryRepository.getAllUsers(query);
   }
 
   @Post()
   async createUser(@Body() dto: UsersInputDto) {
-    return await this.userService.createUser(dto);
+    const createUserAndReturnId: string = await this.userService.createUser(dto);
+
+    return await this.userQueryRepository.getUsersById(createUserAndReturnId);
   }
 
   @Delete(':id')
+  @HttpCode(204)
   async deleteUser(@Param('id') id: string) {
     return await this.userService.deleteUser(id);
   }
+
 }
--- src/modules/user-accounts/infrastructure/user.query-repository.ts
+++ src/modules/user-accounts/infrastructure/user.query-repository.ts
@@ -1,14 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { FilterQuery, Model } from 'mongoose';
 import { User, UserDocument } from '../domain/user.entity';
+import { UserViewDto } from '../api/view-dto/user-view-dto';
+import { PaginatedViewDto } from '../../../core/dto/base.paginated.view-dto';
+import { SortDirection } from '../../../core/dto/base.query-params.input-dto';
+import { GetUsersQueryParams } from '../api/view-dto/get-users-query-params';
 
 @Injectable()
 export class UserQueryRepository {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  async getUsers() {
-    const items = await this.userModel.find();
-    return items;
+  async getUsersById(id: string) {
+    const blog = await this.userModel.findOne({ _id: id });
+    if (!blog) throw new NotFoundException('users not found');
+
+    return UserViewDto.mapToView(blog);
+  }
+
+  async getAllUsers(
+    query: GetUsersQueryParams,
+  ): Promise<PaginatedViewDto<UserViewDto[]>> {
+    const filter: FilterQuery<User> = {};
+
+    if (query.searchLoginTerm) {
+      filter.$or = filter.$or || [];
+      filter.$or.push({
+        login: { $regex: query.searchLoginTerm, $options: 'i' },
+      });
+    }
+
+    if (query.searchEmailTerm) {
+      filter.$or = filter.$or || [];
+      filter.$or.push({
+        email: { $regex: query.searchEmailTerm, $options: 'i' },
+      });
+    }
+    const sortDirection = query.sortDirection ?? SortDirection.Desc;
+
+    const users = await this.userModel.find(filter)
+      .sort({ [query.sortBy]: sortDirection })
+      .skip(query.calculateSkip())
+      .limit(query.pageSize);
+
+    const totalCount = await this.userModel.countDocuments(filter);
+
+    const items = users.map((user) => UserViewDto.mapToView(user));
+
+    return PaginatedViewDto.mapToView({
+      items,
+      totalCount,
+      page: query.pageNumber,
+      size: query.pageSize,
+    });
   }
 }
